3cd4677b4182af6b48ddb2c5c997a90b32f60898
MathieuEnault <enault.mathieu2@gmail.com>
2021-12-07 18:47:01 +0100

    improvment of desktop menu and mobile menu with burger button img switch on click

--- clapp-application/src/app/pages/defibrillators/defibrillators.page.ts
+++ clapp-application/src/app/pages/defibrillators/defibrillators.page.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { HostBinding, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-defibrillators',
@@ -6,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./defibrillators.page.scss'],
 })
 export class DefibrillatorsPage implements OnInit {
+  @HostBinding('class.navbar-opened') navbarOpened = false;
+
+  burgerImg: string = '../../assets/icon/burger-button-icon.png';
+  markImg: string = '../../assets/icon/mark-icon.png';
+
   constructor() {}
 
   ngOnInit() {}
 
-  menuIsOpen() {
-    
+  toggleNavbar() {
+    this.navbarOpened = !this.navbarOpened;
   }
 }
