9c6b4bed4b2e7796163657f44f13313d15e4fc4c
MathieuEnault <enault.mathieu2@gmail.com>
2021-12-12 23:47:18 +0100

    progress regarding the display of the unfolding list on mobile

--- clapp-application/src/app/pages/defibrillators/defibrillators.page.ts
+++ clapp-application/src/app/pages/defibrillators/defibrillators.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
 import { ICustomers } from 'src/app/models/models';
 import { HttpClient } from '@angular/common/http';
-
+// import { Platform } from '@ionic/angular';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-defibrillators',
@@ -9,10 +10,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./defibrillators.page.scss'],
 })
 export class DefibrillatorsPage implements OnInit {
-  customers: ICustomers[];
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  customers: any[]; /* ICustomers[]; */
+  // automaticClose = false;
+  selected: any;
+  @ViewChildren('childItem') childItem;
 
+  // isShown: { [key: number, opened: boolean] } = false;
 
-  constructor(private http: HttpClient) {}
+  // mobileScreen: boolean;
+
+  // screenSize: any = {
+  //   width: 0,
+  //   height: 0,
+  // };
+
+  constructor(private http: HttpClient /* , private platform: Platform */) {
+    // this.getScreenSize();
+    // this.platform.ready().then(() => {
+    //   this.screenSize.width = this.platform.width();
+    //   this.screenSize.height = this.platform.height();
+    // });
+  }
 
   ngOnInit(): void {
     this.http
@@ -20,5 +40,27 @@ export class DefibrillatorsPage implements OnInit {
       .subscribe((data: any[]) => {
         this.customers = data;
       });
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
   }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+  }
+
+  // trackItems(index: number, itemObject: any) {
+  //   return itemObject.id;
+  // }
+
+  toggleSection(index) {
+    this.customers[index].open = !this.customers[index].open;
+  }
+
+  toggleItem(index) {
+    this.customers[index].open = !this.customers[index].open;
+  }
+
+  
 }
