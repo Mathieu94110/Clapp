06739ad83deecdb0e47f87af612b212b8320c0f8
MathieuEnault <enault.mathieu2@gmail.com>
2021-12-13 19:30:39 +0100

    code cleaning

--- clapp-application/src/app/pages/defibrillators/defibrillators.page.ts
+++ clapp-application/src/app/pages/defibrillators/defibrillators.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ViewChildren } from '@angular/core';
-import { ICustomers } from 'src/app/models/models';
 import { HttpClient } from '@angular/common/http';
-// import { Platform } from '@ionic/angular';
 import { HostListener } from '@angular/core';
 
 @Component({
@@ -12,27 +10,10 @@ import { HostListener } from '@angular/core';
 export class DefibrillatorsPage implements OnInit {
   public getScreenWidth: any;
   public getScreenHeight: any;
-  customers: any[]; /* ICustomers[]; */
-  // automaticClose = false;
-  selected: any;
+  customers: any[];
   @ViewChildren('childItem') childItem;
 
-  // isShown: { [key: number, opened: boolean] } = false;
-
-  // mobileScreen: boolean;
-
-  // screenSize: any = {
-  //   width: 0,
-  //   height: 0,
-  // };
-
-  constructor(private http: HttpClient /* , private platform: Platform */) {
-    // this.getScreenSize();
-    // this.platform.ready().then(() => {
-    //   this.screenSize.width = this.platform.width();
-    //   this.screenSize.height = this.platform.height();
-    // });
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http
@@ -50,17 +31,7 @@ export class DefibrillatorsPage implements OnInit {
     this.getScreenHeight = window.innerHeight;
   }
 
-  // trackItems(index: number, itemObject: any) {
-  //   return itemObject.id;
-  // }
-
   toggleSection(index) {
     this.customers[index].open = !this.customers[index].open;
   }
-
-  toggleItem(index) {
-    this.customers[index].open = !this.customers[index].open;
-  }
-
-  
 }
